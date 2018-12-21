3c56f4a576a02950903f035c91f994b4b305eccc
John Wen <John.Wen@ehealth.com>
2018-12-21 14:27:56 +0800

    .

---
+++ findMidSortedArrays.py
@@ -0,0 +1,18 @@
+def findMedianSortedArrays(nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: float
+    """
+    
+    
+
+        
+
+
+if __name__ == "__main__":
+    num1 = [1,5]
+    num2 = [3,8]
+    num3 = num1 + num2
+    num3.sort()
+    print(num3)
